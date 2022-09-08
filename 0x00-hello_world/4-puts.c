a68d17dc20c1697ba8a2ffc2e749f6ec499a846c
Idam-Nkama Nnanna Temple <templeanthony500@gmail.com>
2022-09-08 13:26:02 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * comment
+ * Description: This program prints programming is like building a multilingual puzzle
+ */
+
+int main(void)
+{
+	puts("/"Programming is like building a multilingual puzzle");
+	return(0);
+}
