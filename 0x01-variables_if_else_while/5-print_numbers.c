60cc328634f8841b78388918461fad3bf074c50f
Idam-Nkama Nnanna Temple <templeanthony500@gmail.com>
2022-09-09 15:45:05 -0700

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - prints numbers 0 - 9
+ *
+ * Description: using the main function
+ * this program prints the numbers 0 - 9
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%i", i);
+	}
+	printf("\n");
+	return (0);
+}
