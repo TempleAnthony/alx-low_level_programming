1608d32639ebddc7453d79fb3c02c3055532b62a
Idam-Nkama Nnanna Temple <templeanthony500@gmail.com>
2022-09-08 23:47:09 -0700

    Hello puts

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,7 +2,6 @@
 
 /**
  * main - print the string in the put function
- * 
  * Description: using the main function
  * this program prints with proper grammar, but the outcome is a piece of art,"
  * Return: 0
@@ -10,5 +9,5 @@
 int main(void)
 { 
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return(0);
+	return (0);
 }
