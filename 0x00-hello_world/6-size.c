5365270b21902cb4ad3b8eabefa228cd4dfc4dfc
Idam-Nkama Nnanna Temple <templeanthony500@gmail.com>
2022-09-09 02:27:39 -0700

    Size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,7 +3,7 @@
 /**
  * main - print the string in the put function
  *
- * Description: using the main function
+ * Description: using the main function, return value of main
  * this program prints "Programming is like building a multilingual puzzle
  * Return (0)
  */
@@ -20,5 +20,5 @@ int main(void)
 	printf("Size of a long int: %ld byte(s)\n", sizeof(li));
 	printf("Size of a long long int: %ld byte(s)\n", sizeof(lli));
 	printf("Size of a float: %ld byte(s)\n", sizeof(f));
-	return(0);
+	return (0);
 }
