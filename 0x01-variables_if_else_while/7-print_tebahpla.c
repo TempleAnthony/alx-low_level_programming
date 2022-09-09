78b4601033fd906a9f38e136655500a7bab43745
Idam-Nkama Nnanna Temple <templeanthony500@gmail.com>
2022-09-09 16:00:08 -0700

    tebahpla

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabets backwards
+ *
+ * Description: using the main function
+ * this program prints the letters of the alphabets backwards
+ * Return: 0
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'z' ; c >= 'a' ; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
