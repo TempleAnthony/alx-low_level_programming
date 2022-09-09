15eaa531140a7760183939010594e2cc0dd67942
Idam-Nkama Nnanna Temple <templeanthony500@gmail.com>
2022-09-09 02:31:36 -0700

    Size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,8 +3,9 @@
 /**
  * main - print the string in the put function
  *
- * Description: using the main function, return value of main
+ * Description: using the main function
  * this program prints "Programming is like building a multilingual puzzle
+ * return value of 'main'
  * Return (0)
  */
 int main(void)
