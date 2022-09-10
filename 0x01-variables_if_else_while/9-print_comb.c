c7914437c5af267c2b881840818a38dd40fd9db0
Idam-Nkama Nnanna Temple <templeanthony500@gmail.com>
2022-09-10 01:55:19 -0700

    combination

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - prints a combination of numbers
+ *
+ * Description: using the main function
+ * this program prints the combination of single numbers
+ * Return: 0
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 48; c <= 57; c++)
+	{
+		putchar(c);
+		if (c != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
