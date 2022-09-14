22f5ad48968679d40dc8f5e4199a3463f1cb726a
Idam-Nkama Nnanna Temple <templeanthony500@gmail.com>
2022-09-14 12:55:49 -0700

    loop

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -12,13 +12,7 @@ int main(void)
 	printf("infinite loop incoming :(\n");
 	
 	i = 0;
-
-	while (i < 10)
-	{
-		putchar(i);
-	}
-	/*
-	 * while (i < 10)
+	 /* while (i < 10)
 	 * {
 	 *	 putchar(i);
 	 * }
