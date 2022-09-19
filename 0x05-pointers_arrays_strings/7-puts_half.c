3134231f97a26e959c30a05871debeb702cc030f
Idam-Nkama Nnanna Temple <templeanthony500@gmail.com>
2022-09-19 10:22:17 -0700

    half

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -11,11 +11,18 @@ void puts_half(char *str)
 	int x, y, i;
 
 	x = strlen(str);
+
 	if (x % 2 == 1)
+	{
 		y = x / 2 + 1;
+	}
 	else
+	{
 		y = x / 2;
+	}
 	for (i = y; i < x; i++)
+	{
 		putchar(str[i]);
+	}
 	putchar('\n');
 }
