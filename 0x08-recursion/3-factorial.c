ab1f3a78a28bf2b1715ef1b438351f11889f38fc
Idam-Nkama Nnanna Temple <templeanthony500@gmail.com>
2022-09-28 14:05:47 -0700

    factorial

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -11,13 +11,9 @@ int factorial(int n)
 	int next_factorial;
 
 	if (n < 0)
-	{
 		return (-1);
-	}
-	else if (n == 0);
-	{
+	else if (n == 0)
 		return (1);
-	}
 	next_factorial = factorial(n - 1);
 	return (n * next_factorial);
 }
