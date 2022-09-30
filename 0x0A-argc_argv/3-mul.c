5e4865710d284da100d94fd7d306a84bae85da78
Idam-Nkama Nnanna Temple <templeanthony500@gmail.com>
2022-09-30 15:53:03 -0700

    multiply

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - program that multiplies two numbers
+ * @argc: number of arguments passed to the function
+ * @argv: argument vector of pointers to strings
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[i])
+{
+	int num_1, num_2, mul;
+	
+	if (argc != 3)
+		printf("Error\n");
+	else
+	{
+		num_1 = atoi(argv[1]);
+		num_2 = atoi(argv[2]);
+		mul = num_1 * num_2;
+		printf("%d\n", mul);
+	}
+	return (0);
+}
+
