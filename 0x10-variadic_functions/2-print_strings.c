6bb2165d94d0aa9c81c76b4469a8bf3dc3dd5d7a
Idam-Nkama Nnanna Temple <templeanthony500@gmail.com>
2022-10-14 09:33:49 -0700

    printstring

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -18,7 +18,7 @@ void print_strings(const char *seperator, const unsigned int n, ...)
 	va_start(valist, n);
 	for (i = 0; i < n; i++)
 		{
-			str = va_arg(valist, char *);
+			str = va_arg(valist, char*);
 			if (str)
 				printf("%s", str);
 			else
